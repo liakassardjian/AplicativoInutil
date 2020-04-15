8dfb7f0a0c30c0b3de960f4fa80f0cab41303f20
liakassardjian <liakassardjian@gmail.com>
2020-04-15 15:23:39 -0300

    Criação do MotionManager

---
+++ SensorTesting/SensorTesting/MotionManager.swift
@@ -0,0 +1,39 @@
+//
+//  MotionManager.swift
+//  SensorTesting
+//
+//  Created by Lia Kassardjian on 15/04/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class MotionManager {
+    
+    let motion = CMMotionManager()
+    let gravity = UIGravityBehavior()
+    let collider = UICollisionBehavior()
+    
+    func startDeviceMotion() {
+        if motion.isDeviceMotionAvailable {
+            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
+            self.motion.showsDeviceMovementDisplay = true
+            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+            let timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
+                              block: { (timer) in
+                                if let data = self.motion.deviceMotion {
+                                    let grav = data.gravity
+                            
+                                    let x = CGFloat(grav.x)
+                                    let y = CGFloat(grav.y)
+                                    let v = CGVector(dx: x, dy: -y)
+                                    self.gravity.gravityDirection = v
+                                }
+            })
+            
+            RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+        }
+    }
+
+}
--- SensorTesting/SensorTesting/ViewController.swift
+++ SensorTesting/SensorTesting/ViewController.swift
@@ -11,12 +11,10 @@ import CoreMotion
 
 class ViewController: UIViewController {
  
+    let motionManager = MotionManager()
     
-    let motion = CMMotionManager()
     
     var animator:UIDynamicAnimator? = nil
-    let gravity = UIGravityBehavior()
-    let collider = UICollisionBehavior()
     
     var balls:[UIView] = []
     var yPositions:[CGPoint] = []
@@ -27,12 +25,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        addBalls()
-//        startDeviceMotion()
+        motionManager.startDeviceMotion()
 //        createAnimator()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        motion.stopDeviceMotionUpdates()
+        motionManager.motion.stopDeviceMotionUpdates()
     }
     
     func calculatePositions() {
@@ -70,8 +68,8 @@ class ViewController: UIViewController {
         
         self.view.insertSubview(ball, at: 0)
         
-        collider.addItem(ball)
-        gravity.addItem(ball)
+        motionManager.collider.addItem(ball)
+        motionManager.gravity.addItem(ball)
         
         return ball
     }
@@ -80,47 +78,16 @@ class ViewController: UIViewController {
         animator = UIDynamicAnimator(referenceView: self.view)
         
         // Permite que as bolas colidam com objetos
-        collider.translatesReferenceBoundsIntoBoundary = true
-        animator?.addBehavior(collider)
+        motionManager.collider.translatesReferenceBoundsIntoBoundary = true
+        animator?.addBehavior(motionManager.collider)
         
         // Permite que as bolas apresentem comportamento gravitacional
-        animator?.addBehavior(gravity)
+        animator?.addBehavior(motionManager.gravity)
     }
 
-    func startDeviceMotion() {
-        if motion.isDeviceMotionAvailable {
-            //Frequencia de atualização dos sensores definida em segundos - no caso, 60 vezes por segundo
-            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
-            self.motion.showsDeviceMovementDisplay = true
-            //A partir da chamada desta função, o objeto motion passa a conter valores atualizados dos sensores; o parâmetro representa a referência para cálculo de orientação do dispositivo
-            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
-            
-            //Um Timer é configurado para executar um bloco de código 60 vezes por segundo - a mesma frequência das atualizações dos dados de sensores. Neste bloco manipulamos as informações mais recentes para atualizar a interface.
-            let timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
-                              block: { (timer) in
-                                if let data = self.motion.deviceMotion {
-                                    let grav = data.gravity
-                            
-                                    let x = CGFloat(grav.x)
-                                    let y = CGFloat(grav.y)
-                                    let v = CGVector(dx: x, dy: -y)
-                                    self.gravity.gravityDirection = v
-                                }
-            })
-            
-            RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
-        }
-    }
     
     // Ao tocar na tela, recoloca as bolas em suas posições iniciais
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for i in balls {
-//            i.removeFromSuperview()
-//            gravity.removeItem(i)
-//            collider.removeItem(i)
-//        }
-//        yPositions.removeAll()
-//        addBalls()
         for t in touches {
             let location = t.location(in: self.view)
             print("Started at \(location)")
