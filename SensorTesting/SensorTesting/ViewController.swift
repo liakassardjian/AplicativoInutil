f01bf46ac1dbb4f57a4eb1607874de8210953004
liakassardjian <liakassardjian@gmail.com>
2020-04-15 15:50:20 -0300

    Revisão do código já existente

--- SensorTesting/SensorTesting/ViewController.swift
+++ SensorTesting/SensorTesting/ViewController.swift
@@ -13,15 +13,12 @@ class ViewController: UIViewController {
  
     let motionManager = MotionManager()
     
+    var animator: UIDynamicAnimator?
     
-    var animator:UIDynamicAnimator? = nil
-    
-    var balls:[UIView] = []
-    var yPositions:[CGPoint] = []
+    var balls = [CustomBall]()
+    var randomPositions = [CGPoint]()
     let colors = [#colorLiteral(red: 1, green: 0.1367589235, blue: 0.2771877348, alpha: 1),#colorLiteral(red: 0.8382436633, green: 0, blue: 1, alpha: 1),#colorLiteral(red: 1, green: 0.4022022188, blue: 0, alpha: 1),#colorLiteral(red: 0, green: 1, blue: 0.6779490709, alpha: 1),#colorLiteral(red: 0, green: 0.7733957171, blue: 1, alpha: 1),#colorLiteral(red: 1, green: 0, blue: 0.6808319688, alpha: 1),#colorLiteral(red: 0.1454425454, green: 0.2908638716, blue: 1, alpha: 1),#colorLiteral(red: 0.4930205345, green: 0, blue: 1, alpha: 1),#colorLiteral(red: 1, green: 0.7930418849, blue: 0, alpha: 1),#colorLiteral(red: 1, green: 0.5506727099, blue: 0, alpha: 1)]
-    
-    var ball: UIView?
-   
+       
     override func viewDidLoad() {
         super.viewDidLoad()
         addBalls()
@@ -34,34 +31,28 @@ class ViewController: UIViewController {
     }
     
     func calculatePositions() {
-        var height:CGFloat = 0
-        var width:CGFloat = 0
-        if let view = self.view {
-            height = view.frame.height
-            width = view.frame.width
-        }
+        guard let view = self.view else { return }
         
-       for _ in 0..<colors.count {
-            let randomX = CGFloat.random(in: 0..<width)
-            let randomY = CGFloat.random(in: 0..<height)
+        let height = view.frame.height
+        let width = view.frame.width
+        
+       for _ in colors {
+            let randomX = CGFloat.random(in: 0 ..< width)
+            let randomY = CGFloat.random(in: 0 ..< height)
             let p = CGPoint(x: randomX, y: randomY)
-            yPositions.append(p)
+            randomPositions.append(p)
         }
     }
     
     func addBalls() {
         calculatePositions()
-        for i in 0..<colors.count {
-            let ball = createBall(position: yPositions[i], color: colors[i])
+        for i in 0 ..< colors.count {
+            let ball = createBall(position: randomPositions[i], color: colors[i])
             balls.append(ball)
         }
     }
     
-    func addBall(at position: CGPoint) {
-        ball = createBall(position: position, color: .red)
-    }
-    
-    func createBall(position: CGPoint, color: UIColor) -> UIView {
+    func createBall(position: CGPoint, color: UIColor) -> CustomBall {
         let ball = CustomBall(position: position, color: color)
         
         self.view.insertSubview(ball, at: 0)
