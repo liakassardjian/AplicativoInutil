854c13632880331782f99792fefed9f71cb8a65b
liakassardjian <liakassardjian@gmail.com>
2020-04-17 16:38:28 -0300

    Resolvido problema que fazia com que as esferas não se encostassem totalmente

--- SensorTesting/SensorTesting/CustomBall.swift
+++ SensorTesting/SensorTesting/CustomBall.swift
@@ -42,4 +42,27 @@ class CustomBall: UIView {
         self.layer.insertSublayer(gradient, at: 0)
     }
     
+    var shapeLayer: CAShapeLayer = {
+        let _shapeLayer = CAShapeLayer()
+        _shapeLayer.fillColor = UIColor.clear.cgColor
+        return _shapeLayer
+    }()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.addSublayer(shapeLayer)
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.path = circularPath(center: center).cgPath
+    }
+
+    private func circularPath(lineWidth: CGFloat = 0, center: CGPoint = .zero) -> UIBezierPath {
+        let radius = (min(bounds.width, bounds.height) - lineWidth) / 2
+        return UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: .pi * 2, clockwise: true)
+    }
+
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType { return .path }
+
+    override var collisionBoundingPath: UIBezierPath { return circularPath() }
+    
 }
